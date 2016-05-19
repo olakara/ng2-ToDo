615879f58d1030a92b2c60f1b1ef2141534fa8bd
olakara <Programmerlocal@DSK01-16349.afad.gov.ae>
2016-05-19 09:38:25 +0400

    minor updates for testing codefresh

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -11,7 +11,9 @@ module.exports = function(defaults) {
       'es6-shim/es6-shim.js',
       'reflect-metadata/*.js',
       'rxjs/**/*.js',
-      '@angular/**/*.js'
+      '@angular/**/*.js',
+      'font-awesome/css/*.min.css',
+      'font-awesome/fonts/*.*'
     ]
   });
 };
